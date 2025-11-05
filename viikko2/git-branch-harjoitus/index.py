a1c51d367c153b829ebe7b4668efff87789992c3
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-11-05 12:53:31 +0200

    add tulo

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -1,15 +1,15 @@
-# tehdään alussa importit
-
 from logger import logger
 from summa import summa
 from erotus import erotus
+from tulo import tulo
 
-logger("aloitetaan ohjelma") # muutos mainissa
+logger("aloitetaan ohjelma")
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{x} + {y} = {summa(x, y)}") # muutos mainissa
-print(f"{x} - {y} = {erotus(x, y)}") # muutos mainissa
+print(f"{x} + {y} = {summa(x, y)}") 
+print(f"{x} - {y} = {erotus(x, y)}") 
+print(f"{x} * {y} = {tulo(x, y)}") 
 
 logger("lopetetaan")
-print("goodbye!") # lisäys bugikorjaus-branchissa
+print("goodbye!")
