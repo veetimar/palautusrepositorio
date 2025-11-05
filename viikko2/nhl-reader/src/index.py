525f618eadbc7ec967d4e91db80e5eaf95d3bd7c
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-11-05 11:10:46 +0200

    Add pylint to nhl reader

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,16 +1,21 @@
 import requests
-from rich import print, console, table
+from rich import console
+from rich import table
+from rich import print as rprint
 from player import Player
-input = console.Console().input
+rinput = console.Console().input
 
 class PlayerReader:
     def __init__(self, url):
         self.url = url
 
     def get_players(self):
-        response = requests.get(self.url).json()
+        response = requests.get(self.url, timeout=10).json()
         players = [Player(player_dict) for player_dict in response]
         return players
+
+    def do_nothing(self):
+        pass
 
 class PlayerStats:
     def __init__(self, reader):
@@ -22,25 +27,35 @@ class PlayerStats:
         top = sorted(filtered, key=lambda p: p.points, reverse=True)
         return top
 
+    def do_nothing(self):
+        pass
+
+def make_table(season, nationality):
+    tab = table.Table(title=f"[italic]Season {season} players from {nationality.upper()}[/italic]")
+    tab.add_column("Released", style="dim")
+    tab.add_column("teams", style="dim")
+    tab.add_column("goals", justify="right", style="green")
+    tab.add_column("assists", justify="right", style="green")
+    tab.add_column("points", justify="right", style="green")
+    return tab
+
+def loop(stats, season):
+    while True:
+        nationality = rinput("Nationality [wheat1][USA/FIN/CAN/SWE/CZE/RUS/SLO/FRA/GBR/SVK/DEN/AUT/BLR/GER/SUI/NOR/UZB/LAT/AUS][/wheat1] [cyan]()[/cyan]: ") # pylint: disable=line-too-long
+        players = stats.top_scorers_by_nationality(nationality)
+        tab = make_table(season, nationality)
+        for p in players:
+            tab.add_row(p.name, p.team, str(p.goals), str(p.assists), str(p.points))
+        rprint(tab)
+
 def main():
-    season = input(f"Season [wheat1][2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25/2025-26][/wheat1] [cyan](2024-25)[/cyan]: ")
+    season = rinput("Season [wheat1][2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25/2025-26][/wheat1] [cyan](2024-25)[/cyan]: ") # pylint: disable=line-too-long
     if not season:
         season = "2024-25"
     url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
-    while True:
-        nationality = input("Nationality [wheat1][USA/FIN/CAN/SWE/CZE/RUS/SLO/FRA/GBR/SVK/DEN/AUT/BLR/GER/SUI/NOR/UZB/LAT/AUS][/wheat1] [cyan]()[/cyan]: ")
-        players = stats.top_scorers_by_nationality(nationality)
-        tab = table.Table(title=f"[italic]Season {season} players from {nationality.upper()}[/italic]")
-        tab.add_column("Released", style="dim")
-        tab.add_column("teams", style="dim")
-        tab.add_column("goals", justify="right", style="green")
-        tab.add_column("assists", justify="right", style="green")
-        tab.add_column("points", justify="right", style="green")
-        for p in players:
-            tab.add_row(p.name, p.team, str(p.goals), str(p.assists), str(p.points))
-        print(tab)
+    loop(stats, season)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,14 +1,15 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-        self.nationality = dict['nationality']
-        self.assists = dict['assists']
-        self.goals = dict['goals']
-        self.team = dict['team']
-        self.games = dict['games']
-        if 'id' in dict:
-            self.id = dict['id']
+    def __init__(self, p_dict):
+        self.name = p_dict['name']
+        self.nationality = p_dict['nationality']
+        self.assists = p_dict['assists']
+        self.goals = p_dict['goals']
+        self.team = p_dict['team']
+        self.games = p_dict['games']
         self.points = self.assists + self.goals
+
+    def do_nothing(self):
+        pass
 
     def __str__(self):
         return f"{self.name:20}{self.team:20}{self.goals:2} + {self.assists:2} = {self.points}"
