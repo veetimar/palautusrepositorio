3dd2266cfc5cccff2e1121d2ac73e39b3072ff11
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-11-05 12:28:29 +0200

    modify index

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -8,8 +8,8 @@ logger("aloitetaan ohjelma") # muutos mainissa
 
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+print(f"{x} + {y} = {summa(x, y)}") # muutos mainissa
+print(f"{x} - {y} = {erotus(x, y)}") # muutos mainissa
 
 logger("lopetetaan")
 print("goodbye!") # lisäys bugikorjaus-branchissa
