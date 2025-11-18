3b1779bf811b58c9b04e2df5e190cecf3d42cbb8
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-11-18 19:27:09 +0200

    Implement methods for telling how much stuff is in the cart

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,18 +3,24 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        pass
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+        self.ostoslista = []
 
     def tavaroita_korissa(self):
-        pass
         # kertoo korissa olevien tavaroiden lukumäärän
-        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
+        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
+        total = 0
+        for ostos in self.ostoslista:
+            total += ostos.lukumaara()
+        return total
 
     def hinta(self):
-        return 0
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
+        total = 0
+        for ostos in self.ostoslista:
+            total += ostos.hinta()
+        return total
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
@@ -25,10 +31,10 @@ class Ostoskori:
         pass
 
     def tyhjenna(self):
-        pass
         # tyhjentää ostoskorin
+        pass
 
     def ostokset(self):
-        pass
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
+        pass
