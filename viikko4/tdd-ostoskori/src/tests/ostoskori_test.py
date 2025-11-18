09228d841ef57d80145cc39548a85cb78f252a04
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-11-18 19:45:17 +0200

    Add test for verifying two different products in the cart

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -67,3 +67,12 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
+
+    # step 10
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosoliota(self):
+        maito = Tuote("Maito", 3)
+        juusto = Tuote("Juusto", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(juusto)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 2)
