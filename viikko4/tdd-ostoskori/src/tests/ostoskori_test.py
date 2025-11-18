0bf5c1c13cce161fc459fba6bafae2f3259f5bf6
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-11-18 19:40:13 +0200

    Add test for verifying item count after adding the same product twice to cart

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -33,3 +33,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(juusto)
         self.assertEqual(self.kori.hinta(), 8)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
