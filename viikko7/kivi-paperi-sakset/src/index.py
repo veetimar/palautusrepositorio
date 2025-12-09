0f81728041af477a2ee1617aad446f8b0dcab051
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2025-12-09 19:13:41 +0200

    Refactor index

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -5,38 +5,29 @@ from kps_parempi_tekoaly import KPSParempiTekoaly
 
 def main():
     while True:
-        print("Valitse pelataanko"
-              "\n (a) Ihmistä vastaan"
-              "\n (b) Tekoälyä vastaan"
-              "\n (c) Parannettua tekoälyä vastaan"
-              "\nMuilla valinnoilla lopetetaan"
-              )
-
-        vastaus = input()
-
-        if vastaus.endswith("a"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            kaksinpeli = KPSPelaajaVsPelaaja()
-            kaksinpeli.pelaa()
-        elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            yksinpeli = KPSTekoaly()
-            yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            haastava_yksinpeli = KPSParempiTekoaly()
-            haastava_yksinpeli.pelaa()
-        else:
+        peli = hae_peli()
+        if peli is None:
             break
+        print("Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s")
+        peli.pelaa()
+
+
+def hae_peli():
+    print("Valitse pelataanko"
+          "\n (a) Ihmistä vastaan"
+          "\n (b) Tekoälyä vastaan"
+          "\n (c) Parannettua tekoälyä vastaan"
+          "\nMuilla valinnoilla lopetetaan"
+          )
+    vastaus = input()
+    if vastaus.endswith("a"):
+        return KPSPelaajaVsPelaaja()
+    elif vastaus.endswith("b"):
+        return KPSTekoaly()
+    elif vastaus.endswith("c"):
+        return KPSParempiTekoaly()
+    else:
+        return None
 
 
 if __name__ == "__main__":
